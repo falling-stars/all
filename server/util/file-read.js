6bebe3082124333c24971962ef0471492f9138fd
刘建华 <1769908045@qq.com>
2018-03-28 16:17:34 +0800

    update

--- server/util/file-read.js
+++ server/util/file-read.js
@@ -4,11 +4,7 @@ const path = require('path')
 const read = (filePath, encoding = 'utf8') => {
   return new Promise((resolve, reject) => {
     fs.readFile(path.resolve(__dirname, `../static/${filePath}`), (error, data) => {
-      if (error) {
-        throw error
-      } else {
-        encoding === 'buffer' ? resolve(data) : resolve(data.toString(encoding))
-      }
+      error ? reject(error) : encoding === 'buffer' ? resolve(data) : resolve(data.toString(encoding))
     })
   })
 }
