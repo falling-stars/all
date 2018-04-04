e1f56e5a84115e53d0ca908f5dd4776762bae3de
刘建华 <1769908045@qq.com>
2018-04-04 11:15:45 +0800

    cache

--- server/index.js
+++ server/index.js
@@ -11,15 +11,15 @@ const ssh = {
   cert: fs.readFileSync(resolve(__dirname, './ssh/ssh.pem'))
 }
 
-app.use(server(resolve(__dirname, './static'), {index: "next"}))
+app.use(server(resolve(__dirname, './static'), {index: "next", maxage:10000000000,immutable: true}))
 app.use(router.routes(), router.allowedMethods())
 
 
 https.createServer(ssh, app.callback()).listen(443)
 
-const redirect = new Koa()
-redirect.use(async ctx => {
-  ctx.status = 301
-  ctx.set({'Location': 'https://www.gracly.com'})
-})
-redirect.listen(80)
+// const redirect = new Koa()
+// redirect.use(async ctx => {
+//   ctx.status = 301
+//   ctx.set({'Location': 'https://www.gracly.com'})
+// })
+// redirect.listen(80)
