228347ee6897c238a354054be78f009dc23be497
刘建华 <1769908045@qq.com>
2018-04-04 10:24:15 +0800

    production

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const http = require('http')
 const https = require('https')
+const fs = require('fs')
 const {resolve} = require('path')
 const Koa = require('koa')
 const server = require('koa-static')
 const router = require('./router/index')
 const app = new Koa()
+const ssh = {
+  key: fs.readFileSync(resolve(__dirname, './ssh/ssh.key')),
+  cert: fs.readFileSync(resolve(__dirname, './ssh/ssh.pem'))
+}
 
 app.use(server(resolve(__dirname, './static'), {index: "next"}))
 app.use(router.routes(), router.allowedMethods())
 
-http.createServer(app.callback()).listen(8080,()=>console.log("run in 8080"))
+
+
+https.createServer(ssh, app.callback()).listen(443)
+
+const redirect = new Koa()
+redirect.use(async ctx => ctx.status = 301 || ctx.set("Location", "https://www.gracly.com"))
+redirect.listen(80)
