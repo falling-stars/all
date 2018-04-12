df623fb261473161a5f8b396a80d50686bf483e6
刘建华 <1769908045@qq.com>
2018-04-12 17:41:00 +0800

    dsf

--- client/pages/hub.vue
+++ client/pages/hub.vue
@@ -1,6 +1,6 @@
 <style scoped>
   .head{height: 46px;border-bottom: solid 1px #dfdfdf}
-  .hub{height: 800px;background-color:#eeeeee}
+  .hub{padding: 0.2rem}
   .login{flex-basis: 3.8rem}
   .login *{cursor: pointer}
   .login i{font-size: 22px}
@@ -11,10 +11,11 @@
   .fa-weibo{color:#c80f1e}
   .fa-qq{color:#1E9FFF}
   .liSelect:after{content:'';position:absolute;width:100%;height:3px;bottom:0;left:0;display:inline-block;background-color:#00bc9b}
+  .right{flex-basis: 6rem;margin-left: 0.2rem;height: 880px}
 </style>
 <template>
   <div>
-    <div class="head flex flex-justify-between">
+    <div class="head flex flex-justify-between white-bg">
       <ul class="flex-child-grow padding-left">
         <li class="li-obj inline-block full-height relative" v-for="i in ulList" :class="{liSelect:i.select}">
           <router-link class="inline-block full-height" :to="i.url">{{i.text}}</router-link>
@@ -28,7 +29,10 @@
         <i class="fa fa-qq margin-right" aria-hidden="true"></i>
       </div>
     </div>
-    <div class="hub"></div>
+    <div class="hub flex flex-justify-between grey-e-bg">
+      <div class="left flex-child-grow flex-child-noshrink white-bg">1</div>
+      <div class="right flex-child-noshrink white-bg">2</div>
+    </div>
   </div>
 </template>
 
@@ -36,10 +40,10 @@
   export default {
     data: () => ({
       ulList: [
-        {text: '分享', url: '#', select: true},
-        {text: '2222', url: '#', select: false},
-        {text: '公告', url: '#', select: false},
-        {text: '4444', url: '#', select: false}
+        {text: '首页', url: '#', select: true},
+        {text: '分享', url: '#', select: false},
+        {text: '提问', url: '#', select: false},
+        {text: '公告', url: '#', select: false}
       ]
     })
   }
