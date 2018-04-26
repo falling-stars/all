5545e0285e1b75e581c506792a92818fbbf0bb38
liujianhua <1769908045@qq.com>
2018-04-26 21:13:55 +0800

    fd

---
+++ build/webpack.config-base.js
@@ -0,0 +1,31 @@
+module.exports = {
+  module: {
+    rules: [
+      {test: /\.css$/, loader: ['style-loader', 'css-loader']},
+      {test: /\.js$/, loader: ['babel-loader'], exclude: /node_modules/},
+      {test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader']},
+      {test: /\.less$/, loader: ['style-loader', 'css-loader', 'less-loader']},
+      {test: /\.vue$/, loader: ['vue-loader']},
+      {
+        test: /favicon\.ico$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        exclude: /favicon\.png$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: 'assets/images/[name].[hash].[ext]',
+            limit: 5000
+          }
+        }]
+      }
+    ]
+  }
+}
---
+++ build/webpack.config-pc.js
@@ -0,0 +1,55 @@
+const {resolve} = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const base = require('./webpack.config-base')
+const production = process.env.NODE_ENV === 'production' ? 1 : 0
+module.exports = merge(base, {
+  entry: {
+    bundle: resolve(__dirname, '../pc/main')
+  },
+  output: {
+    path: resolve(__dirname, '../dist-pc'),
+    filename: production ? '[name].js?[chunkhash]' : '[name].js',
+    chunkFilename: production ? '[name].js?[chunkhash]' : '[name].js'
+  },
+  optimization: production ? {
+    splitChunks: {
+      cacheGroups: {
+        manifest: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'manifest',
+          chunks: 'all'
+        }
+      }
+    }
+  } : {},
+  devServer: production ? {} : {
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    contentBase: resolve(__dirname, '../dist-pc'),
+    proxy: {
+      '/api': {
+        target: 'https://m.xxx.com/',
+        changeOrigin: true
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.vue'],
+    alias: {
+      '~': resolve(__dirname, '../pc')
+    }
+  },
+  plugins: [].concat(
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve(__dirname, '../pc/template.html')
+    }),
+    production ? [] : [
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  )
+})
