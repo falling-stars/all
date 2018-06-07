25f18f838d87279bdc6779dc5a64a060dd5a0801
liujianhua <1769908045@qq.com>
2018-06-07 17:24:09 +0800

    editor

---
+++ pc/components/edit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div :id="tag"></div>
+    <button @click="sendContent">get</button>
+  </div>
+</template>
+
+<script>
+  const Edit = require('wangeditor')
+  export default {
+    props: {
+      tag: {
+        type: String,
+        default: ''
+      }
+    },
+    data: () => ({
+      editor: {}
+    }),
+    mounted() {
+      this.editor = new Edit(`#${this.tag}`)
+      this.editor.customConfig.uploadImgShowBase64 = true
+      this.editor.create()
+    },
+    methods: {
+      sendContent() {
+        this.$emit('getContent', this.editor.txt.html())
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
